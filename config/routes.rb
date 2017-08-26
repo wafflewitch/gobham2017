b93a24ecdd11fea71bdbb2f32b4a6384ea21a60e
Jessica Hines <jessicahines717@gmail.com>
2017-08-26 13:07:41 -0500

    added controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  get 'events/new'
+
+  get 'events/show'
+
+  get 'events/edit'
+
+  get 'events/index'
+
+  get 'acts/new'
+
+  get 'acts/show'
+
+  get 'acts/edit'
+
+  get 'acts/index'
+
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/index'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
