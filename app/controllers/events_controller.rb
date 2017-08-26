b2dfd795fdf62c68af76864231aba7c6a585c4ec
Jessica Hines <jessicahines717@gmail.com>
2017-08-26 18:02:49 -0500

    basic controller setup and routes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,61 @@
 class EventsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+  before_action :set_event, only: [ :show, :edit, :update, :destroy ]
+  before_action :set_user, only: [ :new, :create, :index ]
+
   def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = @user
+    if @event.save!
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
   end
 
   def show
+    @user = User.find(@event[:user_id])
+
+    @hash = Gmaps4rails.build_markers(@event) do |event, marker|
+        marker.lat event.latitude if !event.latitude.nil?
+        marker.lng event.longitude if !event.longitude.nil?
+    end
   end
 
   def edit
   end
 
   def index
+    @events = Event.all
+
+    @hash = Gmaps4rails.build_markers(@event) do |event, marker|
+        marker.lat event.latitude if !event.latitude.nil?
+        marker.lng event.longitude if !event.longitude.nil?
+    end
   end
+
+  def destroy
+    @event.destroy
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:user_id, :name, :desc, :location, :latitude, :longitude, :start_time, :end_time, :exp, :max_members, :members)
+  end
+
+  def set_event
+    @event = event.find(params[:id])
+  end
+
+  def set_user
+    @user = User.find(current_user[:id])
+  end
+end
+
 end
