8395cee44a925572ae2308717c11ded39149097d
Katie Ford <katherine.ford@dreamhost.com>
2017-08-26 15:00:44 -0500

    home: create base template file for login/signin
    This is the base template for login and signin.  It is not a finished
    product. It still needs to reconcile variables and urls. This creates an
    initial javascript file but does not source it...yet.

---
+++ app/assets/javascripts/signup.js
@@ -0,0 +1,9 @@
+$(".toggle-signup").click(function() {
+	$('.login-wrapper').hide();
+	$('.signup-wrapper').show();
+}
+
+$(".toggle-login").click(function() {
+	$('.signup-wrapper').hide();
+	$('.login-wrapper').show();
+}
