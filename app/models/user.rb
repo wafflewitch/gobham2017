e78874fefb98b53c3894230c5bba5e9eb35f7f37
Jessica Hines <jessicahines717@gmail.com>
2017-08-26 20:09:45 -0500

    fixing seed for events

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :events
+  has_many :events, :class_name => "Event", :foreign_key => "user_id"
   validates :email, uniqueness: true
   validates :encrypted_password, presence: true
   validates :first_name, presence: true
